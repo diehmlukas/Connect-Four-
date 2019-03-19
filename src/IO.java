bb642951a1bdceaa6589b00cedfa36718ba1d8d4
mac641 <master.tww@web.de>
2019-03-19 17:45:06 +0100

    Whole project update

--- src/IO.java
+++ src/IO.java
@@ -5,33 +5,31 @@ public final class IO {
         System.out.println("Welcome to Collect4!");
         System.out.println("Please take some time to read the rules and this brief description.");
         System.out.println("Rules:");
-        System.out.println("Before you can start playing, please enter your names.");
-
+        System.out.println("In order to start playing, please enter your names.");
     }
 
     public static void printMapHead() {
-        System.out.println("12345678\n");
+        System.out.println("12345678");
     }
 
-    public String readPlayer() throws Exception{
+    public static String readPlayer() throws Exception{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         return bufferedReader.readLine();
     }
 
     public static int readColumn() throws Exception{
-        int in = Integer.parseInt(String.valueOf(System.in.read()));
-        if (in >= 1 && in < 9)
-            return in-1;
-        else
-            return -1;
+        return Integer.parseInt(String.valueOf(System.in.read()));
     }
 
-    public static void printMap(String[][] map, String player){
+    public static void printMap(char[][] map, String[] player){
+        printMapHead();
         for (int i=0; i<8; i++){
             for (int j=0; j<8; j++)
                 System.out.print(map[i][j]);
             System.out.println();
         }
+        printMapHead();
+        System.out.println();
         curPlayer(player);
     }
 
@@ -47,8 +45,8 @@ public final class IO {
         System.out.println("All stones (21) were used! It's a tie!");
     }
 
-    private static void curPlayer(String player) {
+    private static void curPlayer(String[] player) {
         System.out.println();
-        System.out.println(player + " it's your turn!\n");
+        System.out.println(player[0] + " it's your turn! Choose a column to insert your coin (" + player[1] + ").");
     }
 }
--- src/Logic.java
+++ src/Logic.java
@@ -1,18 +1,109 @@
 public class Logic {
-    private String player1;
-    private String player2;
-    private char[][] size;
+    private String[] player1 = new String[2]; //X
+    private String[] player2 = new String[2]; //O
+    private char[][] map;
 
-    public Logic(String player1, String player2){
-        this.player1 = player1;
-        this.player2 = player2;
+    public Logic() throws Exception{
+        map = new char[7][7];
+        this.player1 = new String[2];
+        this.player2 = new String[2];
+        this.player1[1] = "X";
+        this.player2[1] = "O";
+        startGame();
+    }
 
-        size = new char[7][7];
+    private void startGame() throws Exception{
+        IO.printStartText();
+        this.player1[0] = IO.readPlayer();
+        this.player2[0] = IO.readPlayer();
+
         for (int i=0; i<8; i++){
             for (int j=0; j<8; j++)
-                size[i][j] = '.';
+                map[i][j] = '.';
+        }
+        IO.printMap(map, player1);
+    }
+
+    public void execGame() throws Exception{
+        int count = 0;
+        do {
+            int column = checkColumn();
+            if (column != -1) {
+                if (count % 2 == 0) {
+                    insertCoin(column, player1[1].toCharArray()[0]);
+                    checkHelper(player1[0], column);
+                }
+                else {
+                    insertCoin(column, player2[1].toCharArray()[0]);
+                    checkHelper(player2[0], column);
+                }
+            }
+            count++;
+        }while(count<43);
+        IO.tieGame();
+    }
+
+    private void checkHelper(String player, int column) {
+        if (checkCoins(player.toCharArray()[0], column)){
+            IO.win(player);
+            System.exit(0);
         }
     }
 
+    private boolean checkCoins(char sign, int lastColumn) {
+        //get position
+        int i = 0;
+        while(map[i][lastColumn] != sign)
+            i++;
 
+        //check if there's a vertical match
+        boolean out = false;
+        if (i > 2) {
+            for (int j = i; j >= (i-3); j--) {
+                if (map[j][lastColumn] == sign)
+                    out = true;
+                else {
+                    out = false;
+                    break;
+                }
+            }
+        }
+
+        if (out)
+            return out;
+        else { //check if there's a horizontal match
+            for (int j = 0; j < 8; j++) {
+                if (map[i][j] == sign)
+                    out = true;
+                else if(j >= 5 && !out) {
+                    break;
+                } else {
+                    out = false;
+                }
+            }
+        }
+
+        if (out)
+            return out;
+        else { //check if there's a diagonal match
+
+        }
+    }
+
+    private void insertCoin(int column, char sign) {
+        for (int i = 0; i < 8; i++) {
+            if (map[column][i] != '.')
+                map[column][i - 1] = sign;
+        }
+    }
+    
+    private int checkColumn() throws Exception{
+        int column = IO.readColumn();
+        if (column >= 0 && column < 8)
+            return column;
+        else {
+            IO.errorMessage();
+            return -1;
+        }
+    }
 }
